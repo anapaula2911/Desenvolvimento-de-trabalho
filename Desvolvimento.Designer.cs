1d0c93c22680dd3649f74c4078485715396a21e7
Ana Paula Ronquette Porto <anapaularonquette13@gmail.com>
2023-11-29 22:24:35 -0400

    desenvolvimento

---
+++ Desvolvimento.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace CadastroFuncionario.Forms
+{
+    partial class Consultar_fun
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tx_Volta = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tx_Volta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 40);
+            this.panel1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-150, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(878, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "................................................................................." +
+    "...........................";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LIMITLESS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(396, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(409, 46);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Consulta de Funcionário";
+            // 
+            // tx_Volta
+            // 
+            this.tx_Volta.BackgroundColor = System.Drawing.Color.CornflowerBlue;
+            this.tx_Volta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tx_Volta.Location = new System.Drawing.Point(12, 65);
+            this.tx_Volta.Name = "tx_Volta";
+            this.tx_Volta.Size = new System.Drawing.Size(1105, 366);
+            this.tx_Volta.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1, 481);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(1133, 37);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "................................................................................." +
+    "...........................................";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 437);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 66);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Voltar ao Menu";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(631, 449);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(385, 46);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Cadastrar Funcionário:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1041, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 46);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Consultar_Funcionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(1129, 523);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tx_Volta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Consultar_Funcionario";
+            this.Text = "Consultar_Funcionario";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tx_Volta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView tx_Volta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Desvolvimento.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CadastroFuncionario.Uteis;
+
+namespace CadastroFuncionario.Forms
+{
+    public partial class Consultar_fun : Form
+    {
+
+        List<Funcionario> listaFuncionarios = new List<Funcionario>();
+        public Consultar_fun()
+        {
+            InitializeComponent();
+            Consultar();
+        }
+
+        void Consultar()
+        {
+            try
+            {
+                var conexao = new Conexao();
+                var comando = conexao.Comando("SELECT * FROM funcionario");
+                var leitor = comando.ExecuteReader();
+
+                while (leitor.Read())
+                {
+                    var funcionario = new Funcionario();
+                    funcionario.Id = leitor.GetInt32("id_fun");
+                    funcionario.Nome = DAOHelper.GetString(leitor, "nome_fun");
+                    funcionario.Data_nas = DAOHelper.GetDateTime(leitor, "data_nascimento_fun");
+                    funcionario.Cpf = DAOHelper.GetString(leitor, "cpf_fun");
+                    funcionario.Rg = DAOHelper.GetString(leitor, "rg_fun");
+                    funcionario.Telefone = DAOHelper.GetString(leitor, "telefone_fun");
+                    funcionario.Email = DAOHelper.GetString(leitor, "email_fun");
+                    funcionario.Rua = DAOHelper.GetString(leitor, "rua_fun");
+                    funcionario.Bairro = DAOHelper.GetString(leitor, "bairro_fun");
+                    funcionario.Numero = leitor.GetInt32("numero_fun");
+                    funcionario.Complemento = DAOHelper.GetString(leitor, "complemento_fun");
+                    funcionario.Estado_civil = DAOHelper.GetString(leitor, "estado_civil_fun");
+                    funcionario.Funcao = DAOHelper.GetString(leitor, "funcao_fun");
+                    funcionario.Salario = DAOHelper.GetDouble(leitor, "salario_fun");
+                    funcionario.Estado = DAOHelper.GetString(leitor, "estado_fun");
+                    funcionario.Cidade = DAOHelper.GetString(leitor, "cidade_fun");
+
+                    listaFuncionarios.Add(funcionario);
+                }
+                dataGridViewFuncionario.DataSource = listaFuncionarios;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tela_inicial AbrirForms = new Tela_inicial();
+            AbrirForms.Visible = true;
+            this.Close();
+        }
+
+        private void Consultar_fun_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Cadastro_fun abrirForms = new Cadastro_fun();
+            this.Visible = false;
+            abrirForms.ShowDialog();
+
+        }
+    }
+}
